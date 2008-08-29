b40707ce8b606e3caf9a4bf8e17a58398fc17ea0
arno <arno@6c0b4967-9d8a-4ddb-8c85-6a563999f959>
2008-08-29 12:41:43 +0000

    fixing typo
    git-svn-id: http://code.bermilabs.com/projects/plugins/acts_as_sluggable@489 6c0b4967-9d8a-4ddb-8c85-6a563999f959

--- installer/acts_as_sluggable_installer.php
+++ installer/acts_as_sluggable_installer.php
@@ -6,7 +6,7 @@ class ActsAsSluggableInstaller extends AkPluginInstaller
     
     function down_1()
     {
-        echo "Uninstalling the acts_as_taggable plugin migration\n";
+        echo "Uninstalling the acts_as_sluggable plugin migration\n";
     }
     
     function up_1()
